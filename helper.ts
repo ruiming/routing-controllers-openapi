0d35e6331aef0be3e7d94134dce0b06775d283bd
ruiming <ruiming.zhuang@gmail.com>
2018-03-10 15:45:38 +0800

    fix: fix array type

--- helper.ts
+++ helper.ts
@@ -15,7 +15,7 @@ export function Summary(summary: string) {
 
 export function ResType(type: Function | [Function]) {
   return (target: Object, propertyName: string, descriptor: PropertyDescriptor) => {
-    const typename = Array.isArray(type) ? type[0].name : `${type.name}[]`
+    const typename = Array.isArray(type) ? `${type[0].name}[]` : type.name
     Reflect.defineMetadata('design:returntype2', typename, target, propertyName)
   }
 }
